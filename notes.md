79b569acedd299497e861f79bef281635a677b3b
sakshatl <voteforsakshat@gmail.com>
2020-08-18 08:09:47 +0530

    systematic MVC app

---
+++ notes.md
@@ -0,0 +1,32 @@
+# Authentication with JWT (JSON web tokens)
+
+jsonwebtokens are one way to implement authentication in a website
+
+# About setting up mongodb (cloud)
+
+1. go to mongodb.com
+2. create a cluster
+3. create a database 
+4. add collection (name of collection should be plural)
+5. give access rights, create a new user maybe and then give read and write access.
+6. go to connection, and get the connection uri
+
+# App routes (MVC approach)
+
+Model View Contoller approach is a codebase organization approach, means we gonna keep our auth-router handler into seperate folders.
+
+### Auth Routes
+
+1. /signup    GET 
+2. /login     GET
+3. /signup    POST
+4. /login     POST
+5. /logout    GET
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("hello new auth...")
+const express = require('express');
+const mongoose = require('mongoose');
+
+// import routes
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// set view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI = "mongodb+srv://sakshat:test1234@cluster0.ebnyh.mongodb.net/auth-users";
+mongoose.connect(dbURI, {useNewUrlParser :  true, useUnifiedTopology : true, useCreateIndex : true})
+  .then(() => app.listen(5000, () => console.log("server up and running on port : 5000")))
+  .catch((err) => console.log(err))
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+
+
+
+
+
+
