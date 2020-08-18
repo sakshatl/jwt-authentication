5eb2efdeec8d5e3fc8117461b38167b070259ce2
sakshatl <voteforsakshat@gmail.com>
2020-08-18 06:56:39 +0530

    starting all over again

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const verifyAuth = require('../verifyAuth');
-
-router.get('/', verifyAuth, (req, res) => {
-  res.send(req.user); // we have access to the current user.
-})
-
-
-module.exports = router;
